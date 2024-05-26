589f7b33b45b07b8dd22b29881fd597634079317
AStefankiv <stefankif35@gmail.com>
2024-05-25 20:03:31 -0600

    Add search bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,31 @@
+# require 'net/http'
+
+# class HomeController < ApplicationController
+#   def index
+#     # render 'index'
+#     url = "https://api.openweathermap.org/data/2.5/weather?lat=51.049999&lon=-114.066666&units=metric&appid=5d03de9143d8f66e4da9cd4ff4be7cfc"
+#     uri = URI(url)
+#     response = Net::HTTP.get_response(uri)
+#     @data = JSON.parse(response.body)
+#   end
+# end
+
+
 require 'net/http'
 
 class HomeController < ApplicationController
   def index
-    # render 'index'
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=51.049999&lon=-114.066666&units=metric&appid=5d03de9143d8f66e4da9cd4ff4be7cfc"
+    @city = params[:city]&.titleize || "calgary"
+    apiKey = "5d03de9143d8f66e4da9cd4ff4be7cfc"
+    url = "https://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=metric&appid=#{apiKey}"
     uri = URI(url)
     response = Net::HTTP.get_response(uri)
     @data = JSON.parse(response.body)
   end
+
+  def searching
+    @city = params[:city]&.titleize
+    redirect_to action: "index", city: @city
+  end
+
 end
